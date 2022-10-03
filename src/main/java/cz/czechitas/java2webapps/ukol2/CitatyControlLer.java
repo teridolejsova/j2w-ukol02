2c80a4766e99678dee99e67ace7cb5c8cf35c564
Tereza Dolejšová <terdoe>
2022-10-03 17:28:57 +0200

    Hotovo

---
+++ src/main/java/cz/czechitas/java2webapps/ukol2/CitatyControlLer.java
@@ -0,0 +1,43 @@
+package cz.czechitas.java2webapps.ukol2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Controller
+public class CitatyControlLer {
+    List<String> obrazky =List.of("https://source.unsplash.com/j9Kp9Tq-BmE",
+            "https://source.unsplash.com/2PODhmrvLik",
+            "https://source.unsplash.com/QFc2kxpXVYQ",
+            "https://source.unsplash.com/uq2E2V4LhCY",
+            "https://source.unsplash.com/r38u2Uq1AXk",
+            "https://source.unsplash.com/4rDCa5hBlCs",
+            "https://source.unsplash.com/7K24SkQUbSw",
+            "https://source.unsplash.com/EAay7Aj4jbc",
+            "https://source.unsplash.com/KMn4VEeEPR8",
+            "https://source.unsplash.com/4qgJ2E28Uvw");
+
+
+    @GetMapping("/citaty")
+    public ModelAndView nahodneCitaty() throws IOException {
+        Random random = new Random();
+
+        List<String> randomCitaty = FilesReader.readAllLines("citaty.txt");
+        int pocetCitatu = randomCitaty.size();
+        int nahodneCislo = random.nextInt(pocetCitatu);
+        String nahodnyCitat = randomCitaty.get(nahodneCislo);
+
+        String nahodnyObrazek = obrazky.get(random.nextInt(obrazky.size()));
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("nahodnyCitat", nahodnyCitat);
+        modelAndView.addObject("nahodnyObrazek",nahodnyObrazek);
+        return modelAndView;
+    }
+
+
+}
---
+++ src/main/java/cz/czechitas/java2webapps/ukol2/FilesReader.java
@@ -0,0 +1,28 @@
+package cz.czechitas.java2webapps.ukol2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FilesReader {
+    public static List<String> readAllLines(String resource)throws IOException {
+        //Soubory z resources se získávají pomocí classloaderu. Nejprve musíme získat aktuální classloader.
+        ClassLoader classLoader=Thread.currentThread().getContextClassLoader();
+
+        //Pomocí metody getResourceAsStream() získáme z classloaderu InpuStream, který čte z příslušného souboru.
+        //Následně InputStream převedeme na BufferedRead, který čte text v kódování UTF-8
+        try(InputStream inputStream=classLoader.getResourceAsStream(resource);
+            BufferedReader reader=new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))){
+
+            //Metoda lines() vrací stream řádků ze souboru. Pomocí kolektoru převedeme Stream<String> na List<String>.
+            return reader
+                    .lines()
+                    .collect(Collectors.toList());
+        }
+
+    }
+}
